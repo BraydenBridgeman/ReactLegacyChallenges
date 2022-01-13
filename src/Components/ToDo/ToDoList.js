f95ea648626fc417ee2d1363cc1474d416679b9f
Brayden Bridgeman <braydenbridgeman@gmail.com>
2022-01-12 22:08:43 -0500

    fixed a minor issue in To Do List

--- src/Components/ToDo/ToDoList.js
+++ src/Components/ToDo/ToDoList.js
@@ -6,7 +6,7 @@ export default class ToDoList extends Component {
         super();
         this.state = {
             userInput: '',
-            userList: [['', false]]
+            userList: []
         }
     }
 
@@ -28,20 +28,24 @@ export default class ToDoList extends Component {
 
     render() {
         return (
-            <form>
+            <div>
                 <Input placeholder="Add items to your list" onChange={ this.changeUserInput }></Input>
                 <hr />
                 <Button color="primary" onClick={ this.updateList }>Update List</Button>
                 <hr />
-                <label>
+                <div>
+                <h2>
+                    Use the checkbox to complete the task!
+                </h2>
+                <br />
                     {this.state.userList.map((item, index) =>
                         <ul key={index}>
                             <Input type="checkbox" onChange={() => this.completedTask(index)}></Input>
                         <span style={{ display: "inline-block" }}>{!item[1] ? item[0] : <h6>{item}</h6>}</span>
                         </ul>
                     )}
-                </label>
-            </form>
+                </div>
+            </div>
         )
     }
 }
